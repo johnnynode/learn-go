7c0a15c4694198d7e959e736afbe1cc455e02439
johnnynode <johnnynode@gmail.com>
2017-08-23 19:59:56 +0800

    add orginal http test1

--- notes/web/day02.md
+++ notes/web/day02.md
@@ -1,6 +1,6 @@
 ### __Web 学习笔记__
 
-- 在方法中向客户端返回字符串 ：
+- beego 控制器中的方法向客户端返回字符串 ：
  ```
  h.Ctx.WriteString("Hello World")
 
@@ -9,4 +9,20 @@
  打开浏览器：localhost:8080
  显示HelloWorld
 
--
+- 自定义实现http功能
+ ```
+  func main() {
+  	// 设置路由
+  	http.HandleFunc("/", sayHello)
+  	err := http.ListenAndServe(":8080", nil)
+  	if err!=nil{
+  		log.Fatal(err)
+  	}
+  }
+
+  func sayHello(w http.ResponseWriter, r *http.Request) {
+  	io.WriteString(w, "Hello World version1")
+  }
+
+ ```
+
--- src/web-blog/main.go
+++ src/web-blog/main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"github.com/astaxie/beego"
+	"io"
+	"log"
+	"net/http"
 )
 
-type MainController struct {
-	beego.Controller // 在beego 里面默认实现了一些方法,如get, post
-}
-
-func (h *MainController) Get() {
-	h.Ctx.WriteString("Hello World")
-}
-
 func main() {
-	beego.Router("/", &MainController{})
-	beego.Run()
+	// 设置路由
+	http.HandleFunc("/", sayHello)
+	err := http.ListenAndServe(":8080", nil)
+	if err!=nil{
+		log.Fatal(err)
+	}
 }
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello World test say Hello")
+}
+
 
