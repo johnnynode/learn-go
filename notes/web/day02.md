0cbade11174d7b9fe9ea8ebbbabdd2b9257abf64
johnnynode <johnnynode@gmail.com>
2017-08-23 19:30:25 +0800

    test controller method h.Ctx.WriteString

---
+++ notes/web/day02.md
@@ -0,0 +1,12 @@
+### __Web 学习笔记__
+
+- 在方法中向客户端返回字符串 ：
+ ```
+ h.Ctx.WriteString("Hello World")
+
+ ```
+ 运行：$ `go run yourApp.go`
+ 打开浏览器：localhost:8080
+ 显示HelloWorld
+
+-
--- src/web-blog/main.go
+++ src/web-blog/main.go
@@ -1,1 +1,19 @@
-package web_blog
+package main
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type MainController struct {
+	beego.Controller // 在beego 里面默认实现了一些方法,如get, post
+}
+
+func (h *MainController) Get() {
+	h.Ctx.WriteString("Hello World")
+}
+
+func main() {
+	beego.Router("/", &MainController{})
+	beego.Run()
+}
+
